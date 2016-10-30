e2f12cd6263ca1badf5b63460d29d165229ec683
Marshall Guttenberg <mguttenberg@healthright.com>
2016-10-29 21:55:24 -0700

    rearrange static files

--- app/src/index.js
+++ app/src/index.js
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom';
 // import { Provider } from 'react-redux';
 // import store from './store';
 import router from './router';
-import './webflow.css';
-import './index.css';
+import './global/css/webflow.css';
+import './global/css/index.css';
 
 ReactDOM.render(
   router,

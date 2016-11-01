1ebd01d8129606c432d60c9862fb4f1a2683bbee
Marshall Guttenberg <mguttenberg@healthright.com>
2016-10-31 22:17:02 -0700

    move reducers to init

--- app/src/init/reducers.js
+++ app/src/init/reducers.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 
 // Reducers
-import userReducer from './user/reducers';
-import checkoutReducer from './checkout/reducers';
+import userReducer from '../user/reducers';
+import checkoutReducer from '../checkout/reducers';
 
 // Combine Reducers
 var reducers = combineReducers({
